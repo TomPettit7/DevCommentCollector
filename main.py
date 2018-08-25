412208984bfe886f6cd583ef75d93bacc428e666
TomPettit7 <38735580+TomPettit7@users.noreply.github.com>
2018-08-25 21:34:35 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,56 @@
+import praw
+
+r = praw.Reddit(user_agent='The Official DeadByDaylight Bot',
+                client_id='8IaAimMsVSHECg',
+                client_secret='oaeWLkYuXUVKdoeMNfyUtLp0suM',
+                username='TheDBDBot',
+                password='HeyThereDBDCommunity1337')
+
+def dev_comments_collect():
+    devs = ['tozzer7']
+    dev_comments = []
+    ids = []
+    dev_commented = False
+    subreddit = r.subreddit('BotTestingGroundPlace')
+    for submission in subreddit.new():
+        reddit_comment = submission.reply('Here is a list of all comments by developers: \n\n')
+        for comment in submission.comments:
+            if comment.id not in ids:
+                ids.append(comment.id)
+                if str(comment.author) in devs:
+                    dev_commented = True
+                    print('Comment initiated')
+                    comment_body = str(comment.body.encode('utf-8'))
+                    comment_author = comment.author
+
+                    dev_comments.append([comment_author, comment_body])
+
+                    for item in dev_comments:
+                        user = item[0]
+                        body = item[1]
+
+                        string = 'User: ' + str(user) + ' said: ' + str(body) + '\n\n'
+                        with open('comment.txt', 'a') as comment_file:
+                            comment_file.write(string)
+                        with open('comment.txt', 'r') as comment_file_r:
+                            reddit_comment_body = comment_file_r.read()
+
+                        reddit_comment.append(string)
+
+                    reddit_comment.mod.distinguish(how='yes', sticky=True)
+
+                    print('Comment Edited')
+
+                else:
+                    print('Not a dev comment. Still appended')
+
+            else:
+                print('Comment seen already')
+
+        if dev_commented == False:
+            reddit_comment.delete()
+            print('No dev comments found in this thread')
+
+        dev_comments.clear()
+
+dev_comments_collect()
